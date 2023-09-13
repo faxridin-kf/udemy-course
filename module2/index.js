7bfc3a48df2ee9f72774910092a41df5d0927bb7
fakhridin <faxridinkf@gmail.com>
2023-09-13 22:50:35 +0500

    ...

---
+++ module2/index.js
@@ -0,0 +1,11 @@
+const dev = {
+ name: 'Frank',
+ secondName: 'Kuchkarov',
+ skills: ['js', 'ts', 'react'],
+ isMarried: false,
+};
+
+// Use a for...of loop to log each skill
+for (const skill of dev.skills) {
+ console.log(skill);
+}
